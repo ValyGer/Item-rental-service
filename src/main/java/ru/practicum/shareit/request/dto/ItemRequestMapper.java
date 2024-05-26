df7b901c7d13bf25175d847811e561dc19a521de
Valentin <valentin.geras@yandex.ru>
2024-05-26 10:07:59 +0300

    Доработка нового функционала. Добавление методов сервиса, репозитория. Настройки вывода информации.

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestMapper.java
@@ -2,16 +2,22 @@ package ru.practicum.shareit.request.dto;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import ru.practicum.shareit.item.dto.ItemDtoForRequestMapper;
 import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
 
-
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ItemDtoForRequestMapper.class)
 public interface ItemRequestMapper {
 
     @Mapping(source = "id", target = "id")
-    ItemRequest toItemRequest(ItemRequestDto  itemRequestDto);
+    ItemRequest toItemRequest(ItemRequestDto itemRequestDto);
 
     @Mapping(source = "id", target = "id")
-    ItemRequestDto toItemRequestDto(ItemRequest  itemRequest);
-}
+    @Mapping(source = "requester", target = "ownerId", qualifiedBy = RequesterToOwnerIdDto.class)
+    ItemRequestDto toItemRequestDto(ItemRequest itemRequest);
 
+    @RequesterToOwnerIdDto
+    static Long getIdRequester(User requester) {
+        return requester.getId();
+    }
+}
