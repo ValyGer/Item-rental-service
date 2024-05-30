09a1966ad6eb28632945ed9ba6c9df9c2a73ebcb
Valentin <valentin.geras@yandex.ru>
2024-05-30 22:52:13 +0300

    Добавлены тесты сервиса пользователей

--- src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.user;
 
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -9,21 +7,21 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import ru.practicum.shareit.exceptions.ConflictException;
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.user.impl.UserServiceImpl;
 import ru.practicum.shareit.user.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -78,7 +76,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void updateUser_whenUserUpdeted_thenReturnUser() {
+    void updateUser_whenUserUpdated_thenReturnUser() {
         Long userId = 0L;
         User oldUser = new User();
         oldUser.setUserName("Name1");
@@ -89,7 +87,7 @@ class UserServiceImplTest {
         newUser.setEmail("user2@mail.ru");
         when(userRepository.findById(userId)).thenReturn(Optional.of(oldUser));
 
-        User actualUser = userService.updateUser(userId,newUser);
+        userService.updateUser(userId, newUser);
 
         verify(userRepository).save(userArgumentCaptor.capture());
         User savedUser = userArgumentCaptor.getValue();
@@ -99,10 +97,46 @@ class UserServiceImplTest {
     }
 
     @Test
-    void getAllUsers() {
+    void getAllUsers_thenReturnAllUsers() {
+        List<User> allUsers = new ArrayList<>();
+        allUsers.add(new User(1L, "Name1", "user1@mail.ru"));
+        allUsers.add(new User(2L, "Name2", "user2@mail.ru"));
+
+        when(userRepository.findAll()).thenReturn(allUsers);
+
+        List<User> savedAllUsers = userService.getAllUsers();
+
+        assertEquals(savedAllUsers.size(), allUsers.size());
+        assertEquals(savedAllUsers.get(0).getUserName(), "Name1");
+        assertEquals(savedAllUsers.get(0).getEmail(), "user1@mail.ru");
+        assertEquals(savedAllUsers.get(1).getUserName(), "Name2");
+        assertEquals(savedAllUsers.get(1).getEmail(), "user2@mail.ru");
     }
 
     @Test
-    void deleteUser() {
+    void deleteUser_whenUserExists_thenDeletedUser() {
+        Long userId = 0L;
+        User user = new User();
+        user.setUserName("Name1");
+        user.setEmail("user1@mail.ru");
+
+        HttpStatus httpStatus = userService.deleteUser(userId);
+
+        assertEquals(HttpStatus.OK, httpStatus);
+        verify(userRepository, times(1)).deleteById(userId);
+    }
+
+    @Test
+    void deleteUser_whenUserNotExists_thenReturnThrow() {
+        Long userId = 0L;
+        User user = new User();
+        user.setUserName("Name1");
+        user.setEmail("user1@mail.ru");
+
+        doThrow(new RuntimeException())
+                .when(userRepository).deleteById(userId);
+
+        assertThrows(RuntimeException.class,
+                () -> userService.deleteUser(userId));
     }
 }
