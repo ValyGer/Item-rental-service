4a4b7eb6694c209e7c3bbf2fe835ca04ac2a5450
Valentin <valentin.geras@yandex.ru>
2024-06-06 00:05:42 +0300

    Проверка checkStyle

--- src/test/java/ru/practicum/shareit/exceptions/ErrorHandlerTest.java
+++ src/test/java/ru/practicum/shareit/exceptions/ErrorHandlerTest.java
@@ -2,7 +2,7 @@ package ru.practicum.shareit.exceptions;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ErrorHandlerTest {
 
