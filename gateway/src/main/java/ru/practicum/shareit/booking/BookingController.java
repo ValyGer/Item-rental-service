dc5cc23ab037b4e4757ceb7d001fbc61c8a33ec9
Valentin <valentin.geras@yandex.ru>
2024-06-11 17:47:01 +0300

    Исправлены ошибки checkstyle

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 @RestController
